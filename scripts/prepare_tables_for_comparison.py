41353a0351f4461b40e43638d899b78fd291e742
Mark Morley <mmorley0395@gmail.com>
2024-03-20 13:53:57 -0400

    updated based on different column names and capitalization

--- scripts/prepare_tables_for_comparison.py
+++ scripts/prepare_tables_for_comparison.py
@@ -6,61 +6,59 @@ the postgres database so they are ready to be
 compared in 'compare_plans.py'.
 
 """
-import pandas as pd
-import env_vars as ev
 from env_vars import ENGINE
+from sqlalchemy import text
 
 
 def add_codes_to_district_plan():
     add_codes_fields = """
-    ALTER TABLE "District_Plan"
+    ALTER TABLE "DistrictPlan"
     ADD COLUMN IF NOT EXISTS longcode VARCHAR;
 
-    ALTER TABLE "District_Plan"
+    ALTER TABLE "DistrictPlan"
     ADD COLUMN IF NOT EXISTS shortcode VARCHAR;
 
     COMMIT;
     """
 
     populate_codes = """
-    UPDATE "District_Plan"
-    SET shortcode = CONCAT("SR","SegmentFrom", to_char(CAST("SegmentTo" AS numeric), 'fm0000'));
+    UPDATE "DistrictPlan"
+    SET shortcode = CONCAT("State Route","Segment From", to_char(CAST("Segment To" AS numeric), 'fm0000'));
 
 
-    UPDATE "District_Plan"
-    SET longcode = CONCAT("SR","SegmentFrom", "OffsetFrom", to_char(CAST("SegmentTo" AS numeric), 'fm0000'), to_char(CAST("OffsetTo" AS numeric), 'fm0000'));
+    UPDATE "DistrictPlan"
+    SET longcode = CONCAT("State Route","Segment From", "Offset From", to_char(CAST("Segment To" AS numeric), 'fm0000'), to_char(CAST("Offset to" AS numeric), 'fm0000'));
 
     COMMIT;
     """
     con = ENGINE.connect()
-    con.execute(add_codes_fields)
-    con.execute(populate_codes)
+    con.execute(text(add_codes_fields))
+    con.execute(text(populate_codes))
 
 
 def edit_oracle_codes():
     edit_oracle_column_type = """
-    alter table from_oracle 
-    alter column "SHORTCODE" type varchar;
+    alter table oracle_copy 
+    alter column "shortcode" type varchar;
     """
 
     update_oracle_codes = """
-    update from_oracle 
-    set "SHORTCODE" = CONCAT(to_char(CAST("STATE_ROUTE" AS numeric), 'fm0000'), to_char(cast("SEGMENT_FROM" as numeric), 'fm0000'), to_char(cast("SEGMENT_TO" as numeric), 'fm0000'))
+    update oracle_copy 
+    set "shortcode" = CONCAT(to_char(CAST("state_route" AS numeric), 'fm0000'), to_char(cast("segment_from" as numeric), 'fm0000'), to_char(cast("segment_to" as numeric), 'fm0000'))
     ;
 
-    update from_oracle
-    set "LONGCODE" = CONCAT(to_char(CAST("STATE_ROUTE" AS numeric), 'fm0000'), to_char(cast("SEGMENT_FROM" as numeric), 'fm0000'), to_char(cast("OFFSET_FROM" as numeric), 'fm0000'), to_char(cast("SEGMENT_TO" as numeric), 'fm0000'), to_char(cast("OFFSET_TO" as numeric), 'fm0000'))
+    update oracle_copy
+    set "longcode" = CONCAT(to_char(CAST("state_route" AS numeric), 'fm0000'), to_char(cast("segment_from" as numeric), 'fm0000'), to_char(cast("offset_from" as numeric), 'fm0000'), to_char(cast("segment_to" as numeric), 'fm0000'), to_char(cast("offset_to" as numeric), 'fm0000'))
     ;
 
     COMMIT;
     """
     con = ENGINE.connect()
-    con.execute(edit_oracle_column_type)
-    con.execute(update_oracle_codes)
+    con.execute(text(edit_oracle_column_type))
+    con.execute(text(update_oracle_codes))
 
 
 def main():
-    combine_plans_full_district()
     add_codes_to_district_plan()
     edit_oracle_codes()
 
